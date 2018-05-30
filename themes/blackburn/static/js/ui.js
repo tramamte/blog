183db791ddb10eace315559841338a57d13ec5f4
Patrick Yu <tramamte@gmail.com>
2018-05-30 23:17:01 +0900

    update blog data Wed May 30 23:17:01 KST 2018

--- themes/blackburn/static/js/ui.js
+++ themes/blackburn/static/js/ui.js
@@ -6,23 +6,47 @@
         //content  = document.getElementById('content'),
         mask     = document.getElementById('mask');
 
-    function toggleClass(element, className) {
+    function toggleClass(element, name) {
+        var list = element.classList,
+            length = list.length,
+            i = 0
+        
+        if (length !== 0 && list.contains(name)) {
+            list.toggle(name, false);
+        }
+
+        if (length === list.length) {
+            list.add(name);
+        } 
+
+        if (list.length === 0) {
+            element.removeAttribute('class');
+        } else {
+            element.classList = list;
+        }
+
+        /*
         var classes = element.className.split(/\s+/),
             length = classes.length,
             i = 0;
 
         for(; i < length; i++) {
-          if (classes[i] === className) {
+          if (classes[i] === name) {
             classes.splice(i, 1);
             break;
           }
         }
         // The className is not found
         if (length === classes.length) {
-            classes.push(className);
+            classes.push(name);
         }
-
-        element.className = classes.join(' ');
+   
+        if (classes.length === 0) {
+            element.className = '';
+        } else {
+            element.className = classes.join(' ');
+        }
+        */
     }
 
     function toggleAll(e) {
